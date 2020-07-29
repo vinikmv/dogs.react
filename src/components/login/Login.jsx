022ac24acc2be6ceaed1fa58af668d843bfc8aca
Vinicius Vicente <viniciuskmv@gmail.com>
2020-07-29 14:45:00 -0300

    Styles do login adicionado

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styles from './login.module.css';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import LoginForm from './LoginForm';
 import LoginCreate from './LoginForm';
@@ -10,14 +11,16 @@ const Login = () => {
   const { login } = React.useContext(UserContext);
   if (login === true) return <Navigate to="/account" />;
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="create" element={<LoginCreate />} />
-        <Route path="lost" element={<LoginPasswordLost />} />
-        <Route path="reset" element={<LoginPasswordReset />} />
-      </Routes>
-    </div>
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="create" element={<LoginCreate />} />
+          <Route path="lost" element={<LoginPasswordLost />} />
+          <Route path="reset" element={<LoginPasswordReset />} />
+        </Routes>
+      </div>
+    </section>
   );
 };
 
